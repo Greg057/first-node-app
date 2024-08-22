6adceb2976dd23ab1948d4c9ffadfc7a6a44eb14
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-08-22 20:10:06 +0200

    finish and refactor

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import http from 'http'
+import fs from 'fs'
+
+const sendResponse = (filePath, res, statusCode) => {
+    fs.readFile(filePath, (err, data) => {
+        res.writeHead(statusCode, {'Content-Type': 'text/html'})
+        res.end(data)
+    })
+}
 
 const server = http.createServer((req, res) => {
-    res.write("hello q")
-    res.end()
-}).listen(8080)
+    if (req.url === '/') {
+        sendResponse('index.html', res, 200)
+    } else if (req.url === '/about') {
+        sendResponse('about.html', res, 200)
+    } else if (req.url === '/contact-me') {
+        sendResponse('contact-me.html', res, 200)
+    } else {
+        sendResponse('404.html', res, 404)
+    }
+})
+
+server.listen(8080)
